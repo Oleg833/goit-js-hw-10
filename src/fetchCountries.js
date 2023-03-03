b2075bbf7f6187b68af5b035b2078d7ec46f81f2
Oleg833 <oleg.kovtun447@gmail.com>
2023-03-03 04:37:47 +0200

    Country info

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,9 +1,49 @@
-export default countryImport;
+export default fetchCountries;
 
-function countryImport(param) {
-  const arr = {};
+async function fetchCountries(param) {
+  const BASE_URL = `https://restcountries.com/v3.1/name/${param}?fields=name,capital,population,flags,languages`;
+  // const URL_ASK = `https://restcountries.com/v2/name/${param}?fields=name,capital,currencies`;
 
-  fetch('https://restcountries.com/v3.1/name/${param}').then(res =>
-    console.log(res)
-  );
+  const response = await fetch(BASE_URL);
+  const res = await response.json();
+
+  return res;
 }
+
+// fetch(URL_ASK).then(response => {
+//   if (!response.ok) {
+//     throw new Error(response.status);
+//   }
+//   return response.json();
+// });
+
+// async function countryImport(param) {
+//   const BASE_URL = 'https://restcountries.com/v3.1/name/';
+//   const URL_ASK = `${BASE_URL}${param}`;
+
+//   const response = await fetch(URL_ASK);
+//   const res = await response.json();
+//   const res_1 = [...res[0].altSpellings];
+//   console.log(res_1);
+//   console.log(res_1.flatMap(my => [my]).join(' '));
+//   return res_1.flatMap(my_1 => [my_1]).join(' ');
+// }
+
+// let myRes;
+
+// return
+//   fetch(URL_ASK)
+//     .then(response => response.json())
+//     .then(res => [...res[0].altSpellings])
+//     .then(res => {
+//       console.log(res);
+//       console.log(res.flatMap(my => [my]).join(' '));
+//       return res.flatMap(my => [my]).join(' ');
+//     });
+
+// .then(reses => reses.map(my => [...my]))
+// .then(res => console.log(res));
+
+//   fetch(URL_ASK)
+//     .then(response => response.text())
+//     .then(res => console.log(res));
--- src/index.js
+++ src/index.js
@@ -3,10 +3,129 @@ import Notiflix from 'notiflix';
 import debounce from 'lodash.debounce';
 // import throttle from 'lodash.throttle';
 
+import fetchCountries from './fetchCountries';
 const DEBOUNCE_DELAY = 300;
+
+const form = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+// form.addEventListener('input', debounce(onInput, 300));
+form.addEventListener('input', onInput);
+
+function onInput(event) {
+  debounce(
+    fetchCountries(event.currentTarget.value)
+      .then(users => {
+        if (users.length > 10) {
+          Notiflix.Notify.info(
+            'Too many matches found. Please enter a more specific name.'
+          );
+          return users;
+        }
+        if (users.length === 1) {
+          countryList.innerHTML = '';
+          renderUserList(users);
+          return users;
+        }
+        countryInfo.innerHTML = '';
+        fastRender(users);
+        console.log(`Found:`, users.length, `country`);
+        return users;
+      })
+      .then(res => {
+        console.log(
+          `Country name:`,
+          res[0].name.official.toString(),
+          ` Capital:`,
+          res[0].capital.toString(),
+          ` Population:`,
+          res[0].population.toString(),
+          ` languages:`,
+          Object.values(res[0].languages).join(',').toString()
+        );
+      })
+      .catch(error =>
+        Notiflix.Notify.failure('Oops, there is no country with that name')
+      ),
+    DEBOUNCE_DELAY
+  );
+}
+
+function fastRender(users) {
+  const markup = users
+    .map(user => {
+      return `<li>      
+      <p>
+      <img src="${user.flags.png.toString()}" width="25" alt="png">
+          <b>Country name</b>: ${user.name.official.toString()}</p>          
+        </li>`;
+    })
+    .join('');
+  countryList.innerHTML = markup;
+}
+function renderUserList(users) {
+  const markup = users
+    .map(user => {
+      return `<li>      
+      <p>
+      <img src="${user.flags.png.toString()}" width="40" alt="png">
+          <b>Country name</b>: ${user.name.official.toString()}</p>
+          <p><b>Capital</b>: ${user.capital.toString()}</p>
+          <p><b>Population</b>: ${user.population.toString()}</p>
+          <p><b>languages</b>: ${Object.values(user.languages)
+            .join(',')
+            .toString()}</p>
+        </li>`;
+    })
+    .join('');
+  countryInfo.innerHTML = markup;
+}
+
+{
+  /* <svg width="170" height="170">
+  <use href="${user.flags.svg.toString()}"></use>
+</svg>; */
+}
+
+// fetchCountries('jap')
+//   .then(users => {
+//     console.log(`All found = `, users);
+//     return users;
+//   })
+//   .catch(error => console.log(error))
+//   .then(res => {
+//     console.log(
+//       `Country name:` + res[0].name.official.toString(),
+//       `languages:` + Object.values(res[0].languages).join(',').toString(),
+//       `Capital:` + res[0].capital.toString(),
+//       `Population:` + res[0].population.toString()
+//     );
+//   });
+
+// console.log('Stop');
+
+// const settings = {
+//   theme: 'dark',
+//   isAuthenticated: true,
+//   options: [1, 2, 3],
+// };
+// const LOCALSTORAGE_KEY = 'settings';
+// localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(settings));
+
+// const savedSettings = localStorage.getItem(LOCALSTORAGE_KEY);
+// const parsedSettings = JSON.parse(savedSettings);
+// console.log(parsedSettings); // settings object
+
+// const res_1 = [...res[0].altSpellings];
+// console.log(res_1);
+// console.log(res_1.flatMap(my => [my]).join(' '));
 
 // fetch('https://jsonplaceholder.typicode.com/todos/1')
 //   .then(response => response.json())
+//   .then(json => console.log(json));
+
+// fetch('https://jsonplaceholder.typicode.com/albums')
+//   //   .then(response => response.json())
 //   .then(json => console.log(json));
 
 // const BASE_URL = 'https://pokeapi.co/api/v2/';
@@ -19,10 +138,10 @@ const DEBOUNCE_DELAY = 300;
 // fetch('https://restcountries.com/v3.1/name/peru').then(res =>
 //   Notiflix.Notify.success(`${res}`)
 // );
-function myCountry(param) {
-  const BASE_URL = 'https://restcountries.com/v3.1/name/';
-  const URL_ASK = `${BASE_URL}${param}`;
-  fetch(URL_ASK).then(res => console.log(res));
-}
+// function myCountry(param) {
+//   const BASE_URL = 'https://restcountries.com/v3.1/name/';
+//   const URL_ASK = `${BASE_URL}${param}`;
+//   fetch(URL_ASK).then(res => console.log(res));
+// }
 
-myCountry('peru');
+// myCountry('peru');
